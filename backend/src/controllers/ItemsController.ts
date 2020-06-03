c8f05dc4286ef1cc6ecd4296c5733dfcd249a974
Cômico <bitotajima@hotmail.com>
2020-06-03 14:29:55 -0300

    Dia 2 - Back-end finalizado

---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,17 @@
+import knex from '../database/connection';
+import { Request, Response } from 'express';
+
+export default class ItemsController {
+    async index (req: Request, res: Response) {
+        const items = await knex('items').select('*');
+    
+        const serializedItems = items.map(item => {
+            return {
+                id: item.id,
+                title: item.title,
+                image_url: `http://localhost:3333/uploads/${item.image}`
+            }
+        });
+        return res.json(serializedItems);
+    }
+}
